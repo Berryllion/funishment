816d0ee112a28e31a283c9f6872e62193a7fcb61
Tatiana <tatiana.relic@gmail.com>
2022-11-15 21:54:10 +0100

    chore: comment unused code

--- pages/items.tsx
+++ pages/items.tsx
@@ -1,47 +1,43 @@
-import { useState } from "react"
-import styled from "@emotion/styled"
+// import { useState } from "react"
+// import styled from "@emotion/styled"
 import { useRouter } from "next/router"
 
-import cards from "../json/cards.json"
+// import cards from "../json/cards.json"
 
 import Main from "../design-system/Main"
-import { Button, ButtonsContainer, RoleButton, RoleButtonsContainer } from "../design-system/Buttons"
-import { emphasizeWords } from "../utils/text-manipulation"
+// import { Button, ButtonsContainer, RoleButton, RoleButtonsContainer } from "../design-system/Buttons"
+// import { emphasizeWords } from "../utils/text-manipulation"
 
 // const xRatedExtensions = false
 
-let homeItems: Array<string> = []
-let sexToysItems: Array<string> = []
+// let homeItems: Array<string> = []
+// let sexToysItems: Array<string> = []
 
-// TODO: remove x-rated cards before parsing items
-cards.map(card => {
-  // if (!xRatedExtensions && card.type === "x-rated-command") return
+// // TODO: remove x-rated cards before parsing items
+// cards.map(card => {
+//   // if (!xRatedExtensions && card.type === "x-rated-command") return
 
-  const cardSpecialItems = card["specialItems"] ?? null
+//   const cardSpecialItems = card["specialItems"] ?? null
 
-  if (cardSpecialItems) {
-    cardSpecialItems.map((items: any) => {
-      if (typeof items === "string") return
+//   if (cardSpecialItems) {
+//     cardSpecialItems.map((items: { type: string, items: Array<string> }) => {
+//       if (items.type === "home") homeItems.push(...items.items)
+//       if (items.type === "sex-toys") sexToysItems.push(items.items)
+//     })
+//   }
+// })
 
-      if (items.type === "home") homeItems.push(...items.items)
-      if (items.type === "sex-toys") sexToysItems.push(items.items)
-    })
-  }
-})
-
-const noDuplicatesHomeItems = new Set([...homeItems])
-const noDuplicatesSexToysItems = new Set([...sexToysItems])
+// const noDuplicatesHomeItems = new Set([...homeItems])
+// const noDuplicatesSexToysItems = new Set([...sexToysItems])
 
 export default function ContentWarning() {
-  const [xRated, setXRated] = useState(false)
-
   const router = useRouter()
 
   const goToSafewords = () => router.push("/safewords")
 
   return (
     <Main>
-      <h2>Items</h2>
+      {/* <h2>Items</h2>
 
       <h3>Home</h3>
       <ul>
@@ -58,7 +54,7 @@ export default function ContentWarning() {
         >
           Continue
         </Button>
-      </ButtonsContainer>
+      </ButtonsContainer> */}
     </Main>
   )
 }
